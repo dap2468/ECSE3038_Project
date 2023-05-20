3e5cb40b6b0aed85c4a26edfc1ff5d3fa3b6fc7b
dap2468 <dap2468@gmail.com>
2023-05-20 17:17:43 -0500

    First commit

---
+++ api/app.py
@@ -0,0 +1,149 @@
+from fastapi import FastAPI,HTTPException,Request
+from bson import ObjectId
+import motor.motor_asyncio
+from fastapi.middleware.cors import CORSMiddleware
+import pydantic
+import os
+from dotenv import load_dotenv
+from datetime import datetime,timedelta
+import requests
+import re
+
+
+load_dotenv()
+app= FastAPI()
+
+origins=["https://simple-smart-hub-client.netlify.app"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+client= motor.motor_asyncio.AsyncIOMotorClient(os.getenv('DOMAIN'))
+db = client.status_database 
+
+pydantic.json.ENCODERS_BY_TYPE[ObjectId]=str
+
+
+#SUNSET FUNCTION
+def sunset():
+    get_sunset=requests.get(f'https://api.sunrise-sunset.org/json?lat=18.1096&lng=-77.2975&date=today')
+    s_json = get_sunset.json()
+    time_date = s_json["results"]["sunset"] #Returns Sunset in UTC Time
+    time_date = datetime.strptime(time_date,'%I:%M:%S %p') + timedelta(hours=-5) #Converting form UTC to GMT-5 (Our Timezone)
+    time_date = datetime.strftime(time_date,'%H:%M:%S') 
+    return time_date
+
+#PARSE TIME FUNCTION
+regex = re.compile(r'((?P<hours>\d+?)h)?((?P<minutes>\d+?)m)?((?P<seconds>\d+?)s)?')
+
+def parse_time(time_str):
+    parts = regex.match(time_str)
+    if not parts:
+        return
+    parts = parts.groupdict()
+    time_params = {}
+    for name, param in parts.items():
+        if param:
+            time_params[name] = int(param)
+    return timedelta(**time_params)
+
+###########################################################################################################################
+#                                           ESP REQUESTS
+###########################################################################################################################
+
+#POST  
+@app.post("/api/status",status_code=201)
+async def state_entry(request:Request):
+    
+    status = await request.json()
+    status["date_time"]=(datetime.now()+timedelta(hours=-5)).strftime('%Y-%m-%dT%H:%M:%S')
+
+    new = await db["status"].insert_one(status)
+    update = await db["status"].find_one({"_id": new.inserted_id })
+    if new.acknowledged == True:
+        return update
+    raise HTTPException(status_code=400,detail="Server cannot process the request due to something that is perceived to be a client error")
+
+
+#GET
+@app.get("/api/status")
+async def getstate():
+    status_now = await db["status"].find().sort("datetime",-1).to_list(1)
+    settings_now = await db["settings"].find().to_list(1)
+
+    dis_sensor = status_now[0]["presence"]
+    time=datetime.strptime(datetime.strftime(datetime.now()+timedelta(hours=-5),'%H:%M:%S'),'%H:%M:%S')
+    user_time=datetime.strptime(settings_now[0]["user_light"],'%H:%M:%S')
+    off_time=datetime.strptime(settings_now[0]["light_off_time"],'%H:%M:%S')
+
+    fan = ((float(status_now[0]["temperature"])>float(settings_now[0]["user_temp"])) and dis_sensor)
+    light = (time>user_time) and (dis_sensor) and (time<off_time)
+    
+    states ={"fan":fan, "light":light}
+    return states
+
+
+
+###########################################################################################################################
+#                                           Webpage REQUESTS
+###########################################################################################################################
+
+#GET
+@app.get("/graph", status_code=200)
+async def graphpoints(request:Request,size: int):
+    n = size
+    list_of_status = await db["status"].find().sort("datetime",-1).to_list(n)
+    list_of_status.reverse()
+    return list_of_status
+
+#PUT
+@app.put("/settings",status_code=200)
+async def setting(request:Request):
+    
+    setting = await request.json()
+    variables = await db["settings"].find().to_list(1)
+    modified = {}
+    modified["user_temp"]=setting["user_temp"]
+    if setting["user_light"]== "sunset":
+        modified["user_light"]=sunset()
+    else:
+        modified["user_light"] = setting["user_light"]
+    
+    modified["light_off_time"]= (datetime.strptime(modified["user_light"],'%H:%M:%S')+parse_time(setting["light_duration"])).strftime("%H:%M:%S")
+    
+
+    if len(variables)==0:
+         new_setting = await db["settings"].insert_one(modified)
+         patched_setting = await db["settings"].find_one({"_id": new_setting.inserted_id })
+         return patched_setting
+    else:
+        id=variables[0]["_id"]
+        updated_setting= await db["settings"].update_one({"_id":id},{"$set": modified})
+        patched_setting = await db["settings"].find_one({"_id": id})
+        if updated_setting.modified_count>=1: 
+            return patched_setting
+    raise HTTPException(status_code=400,detail="Server cannot process the request due to something that is perceived to be a client error")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ embeded/src/main.cpp
@@ -0,0 +1,165 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include "env.h"
+
+#define light 21
+#define fan 19
+#define dis_sensor 3
+#define temp_sensor 2
+
+OneWire oneWire(temp_sensor);	
+DallasTemperature sensors(&oneWire);
+
+
+
+void setup() {
+
+  pinMode(light, OUTPUT);
+  pinMode(fan, OUTPUT);
+  pinMode(dis_sensor, INPUT);
+  pinMode(temp_sensor,INPUT);
+
+  Serial.begin(9600);
+  WiFi.begin(WIFI_SSID,WIFI_PASS);
+
+  Serial.println("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (WiFi.status() == WL_CONNECTED){
+    
+
+    
+
+    //Post Request
+    HTTPClient http;
+  
+    // Establish a connection to the server
+    http.begin(postrequest);
+
+    
+
+    http.addHeader("Content-Type", "application/json");
+
+    // Serialise JSON object into a string to be sent to the API
+    StaticJsonDocument<1000> doc;
+    String httpRequestData;
+
+
+
+    // Send the command to get temperatures
+    sensors.requestTemperatures(); 
+
+    //print the temperature in Celsius
+    Serial.print("Temperature: ");
+    Serial.println(sensors.getTempCByIndex(0));
+
+    Serial.print("Distance: ");
+    Serial.println(digitalRead(dis_sensor));
+
+
+    doc["presence"] = digitalRead(dis_sensor);
+    doc["temperature"] = sensors.getTempCByIndex(0);
+
+    serializeJson(doc, httpRequestData);
+
+
+    // Send HTTP POST request
+    int httpResponseCode = http.PUT(httpRequestData);
+    String http_response;
+
+    // check reuslt of POST request. negative response code means server wasn't reached
+     
+   
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+
+    http_response = http.getString();
+    Serial.println(http_response);
+      
+    // Free resources
+      http.end();
+
+      delay(2000);
+
+    
+    
+
+
+    //GET Request
+    HTTPClient http;
+
+    String http_response;
+
+    http.begin(getrequest);
+
+    int httpResponseCode = http.GET();
+
+    if(httpResponseCode>0){
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+
+      Serial.print("Response from server: ");
+      http_response = http.getString();
+      
+
+    }
+    else {
+      Serial.print("Error Code: ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end();
+
+    // Stream& input;
+
+    StaticJsonDocument<1000> doc;
+
+    DeserializationError error = deserializeJson(doc, http_response);
+
+    if (error) {
+      Serial.print("deserializeJson() failed: ");
+      Serial.println(error.c_str());
+      return;
+    }
+
+    bool light_switch = doc["light"];
+    bool fan_switch = doc["fan"];
+
+    Serial.println("");
+
+    Serial.print("light_switch: ");
+    Serial.println(light_switch);
+    
+
+    Serial.print("fan_switch: ");
+    Serial.println(fan_switch);
+
+    Serial.println("");
+
+    //switch 1
+    digitalWrite(light,light_switch);
+
+    //switch 2
+    digitalWrite(fan,fan_switch);
+
+  }
+
+  else {
+    return;
+  }
+}
